5595b1c3644c742321816bf3b67be1bbacf20907
Vasiliy Kostrubov <v.kostrubov@hotmail.com>
2020-06-09 23:58:28 +0300

    added gpio first interaction try

--- home_service/app.py
+++ home_service/app.py
@@ -7,5 +7,5 @@ import os
 config_path = os.environ.get("SERVER_CONFIG_PATH", "configuration/home_service/server.yaml")
 
 config = Configuration(config_path)
-server = ControlServer(config["server"]["host"], int(config["server"]["port"]))
+server = ControlServer(config["server"]["host"], int(config["server"]["port"]), config=config)
 asyncore.loop()
--- home_service/src/server.py
+++ home_service/src/server.py
@@ -1,22 +1,45 @@
 import asyncore
 import struct
+import gpiozero
 
+raspberry = False
+try:
+    gpiozero.pi_info()
+    raspberry = True
+except:
+    print("This is not raspberry")
 
 class ControlHandler(asyncore.dispatcher_with_send):
 
+    def __init__(self, sock=None, map=None, config={}):
+        super(ControlHandler, self).__init__(sock)
+        self.config = config
+
     def handle_read(self):
+        """
+        Receives a data structure
+        < - little-endian
+        0 - h - integer (short 2 bytes) - id
+        1 - ? - bool (Bool 1 byte) - on/off (0/1)
+        """
         data = self.recv(8192)
         print(data)
         if data:
             data = struct.unpack("<h?", data)
             print(data)
+            bcm_pin = self.config["bindings"][data[0]]["bcm_pin"]
+            print(self.config["bindings"][data[0]])
+            if raspberry:
+                relay = gpiozero.LED(bcm_pin)
+            
             self.send(str(data).encode())
         
 
 class ControlServer(asyncore.dispatcher):
 
-    def __init__(self, host, port):
+    def __init__(self, host, port, **kwargs):
         asyncore.dispatcher.__init__(self)
+        self.config = kwargs.get("config", {})
         self.ignore_log_types = []
         self.create_socket()
         self.set_reuse_addr()
@@ -25,4 +48,5 @@ class ControlServer(asyncore.dispatcher):
 
     def handle_accepted(self, sock, addr):
         print('Incoming connection from %s' % repr(addr))
-        handler = ControlHandler(sock)
+        # handler = ControlHandler(sock=sock, config=self.config)
+        handler = ControlHandler(sock, config=self.config)
