946b72025a74fb3190d8c93a3c1af52e8d4120d3
Vasiliy Kostrubov <v.kostrubov@hotmail.com>
2020-06-10 19:10:58 +0300

    controllers fixed

--- home_app/src/controller_app.py
+++ home_app/src/controller_app.py
@@ -1,10 +1,17 @@
-from controller import Controller
+from home_core.src.models.controller import Controller
+import struct
+import socket
 
 class ControllerApp(Controller):
-    def on():
+    def pack_data(self, activate=False):
+        data = struct.pack(self.struct_template, self.pin, activate)
+        return data
+
+    def on(self):
         # TODO implement message sending
         pass
 
-    def off():
+
+    def off(self):
         # TODO implement message sending
         pass
--- home_service/src/models/controller_service.py
+++ home_service/src/models/controller_service.py
@@ -1,11 +1,14 @@
+import struct
+
 from home_core.src.models.controller import Controller
+
 
 class ControllerService(Controller):
     def __init__(self, bcm_pin, name=None):
         super(ControllerService).__init__(self, bcm_pin, name)
         self.relay = gpio.LED(bcm_pin)
 
-    def unpack_data(value):
+    def unpack_data(self, value):
         data = struct.unpack(self.struct_template, value)
         bcm_pin = data[0]
         activate = data[1]
